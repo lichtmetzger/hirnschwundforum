a3743e7e2845162ce4c24981a439a5c966d31abd
Danny Schmarsel <danny@schmarsel.de>
2016-08-17 20:55:58 +0200

    SoftDelete v1.0.3

--- include/functions.php
+++ include/functions.php
@@ -727,9 +727,41 @@ function delete_avatar($user_id)
 
 
 //
-// Delete a topic and all of its posts
+// Recycle a topic
 //
 function delete_topic($topic_id)
+{
+	global $db, $pun_user, $pun_config;
+
+	// Verify that the move to forum ID is valid
+	$result = $db->query('SELECT 1 FROM '.$db->prefix.'forums AS f LEFT JOIN '.$db->prefix.'forum_perms AS fp ON (fp.group_id='.$pun_user['g_id'].' AND fp.forum_id='.$pun_config['o_softdelete_forum'].') WHERE f.redirect_url IS NULL AND (fp.post_topics IS NULL OR fp.post_topics=1)') or error('Unable to fetch forum permissions', __FILE__, __LINE__, $db->error());
+	if (!$db->num_rows($result))
+		message($lang_common['Bad request']);
+
+	$result = $db->query('SELECT forum_id FROM '.$db->prefix.'topics WHERE id='.$topic_id) or error('Unable to fetch topic information');
+	$topic = $db->fetch_assoc($result);
+
+	if (intval($pun_config['o_softdelete_forum']) > 0 && $topic['forum_id'] != $pun_config['o_softdelete_forum']) {
+		if ($topic['forum_id'] != $pun_config['o_softdelete_forum']) {
+			// Delete any redirect topics
+			$db->query('DELETE FROM '.$db->prefix.'topics WHERE moved_to='.$topic_id) or error('Unable to delete redirect topics', __FILE__, __LINE__, $db->error());
+		
+			$db->query('UPDATE '.$db->prefix.'topics SET forum_id='.$pun_config['o_softdelete_forum'].' WHERE id='.$topic_id) or error('Unable to move topic', __FILE__, __LINE__, $db->error());
+
+			update_forum($topic['forum_id']);
+			update_forum($pun_config['o_softdelete_forum']);
+		} else if ($pun_user['g_id'] == PUN_ADMIN) {
+			hard_delete_topic($topic_id);
+		}
+	} else {
+		hard_delete_topic($topic_id);
+	}
+}
+
+//
+// Delete a topic and all of its posts
+//
+function hard_delete_topic($topic_id)
 {
 	global $db;
 
@@ -760,9 +792,53 @@ function delete_topic($topic_id)
 
 
 //
-// Delete a single post
+// Split a post and recycle
 //
 function delete_post($post_id, $topic_id)
+{
+	global $db, $pun_user, $pun_config;
+
+	// Verify that the move to forum ID is valid
+	$result = $db->query('SELECT 1 FROM '.$db->prefix.'forums AS f LEFT JOIN '.$db->prefix.'forum_perms AS fp ON (fp.group_id='.$pun_user['g_id'].' AND fp.forum_id='.$pun_config['o_softdelete_forum'].') WHERE f.redirect_url IS NULL AND (fp.post_topics IS NULL OR fp.post_topics=1)') or error('Unable to fetch forum permissions', __FILE__, __LINE__, $db->error());
+	if (!$db->num_rows($result))
+		message($lang_common['Bad request']);
+
+	$result = $db->query('SELECT p.id, p.poster, p.posted, t.forum_id AS forum_id FROM '.$db->prefix.'posts AS p INNER JOIN '.$db->prefix.'topics AS t ON p.topic_id = t.id WHERE p.id='.$post_id) or error('Unable to fetch post info', __FILE__, __LINE__, $db->error());
+	$post = $db->fetch_assoc($result);
+
+	if (intval($pun_config['o_softdelete_forum']) > 0) {
+		if ($post['forum_id'] != $pun_config['o_softdelete_forum']) {
+			// Create the new topic
+			$db->query('INSERT INTO '.$db->prefix.'topics (poster, subject, posted, first_post_id, forum_id) VALUES (\''.$db->escape($post['poster']).'\', \'p'.intval($post['id']).'\', '.$post['posted'].', '.$post['id'].', '.$pun_config['o_softdelete_forum'].')') or error('Unable to create new topic', __FILE__, __LINE__, $db->error());
+			$new_tid = $db->insert_id();
+
+			// Move the posts to the new topic
+			$db->query('UPDATE '.$db->prefix.'posts SET topic_id='.$new_tid.' WHERE id='.$post['id']) or error('Unable to move post into new topic', __FILE__, __LINE__, $db->error());
+
+			// Get last_post, last_post_id, and last_poster from the topic and update it
+			$result = $db->query('SELECT id, poster, posted FROM '.$db->prefix.'posts WHERE topic_id='.$topic_id.' ORDER BY id DESC LIMIT 1') or error('Unable to fetch post info', __FILE__, __LINE__, $db->error());
+			$last_post_data = $db->fetch_assoc($result);
+			$db->query('UPDATE '.$db->prefix.'topics SET last_post='.$last_post_data['posted'].', last_post_id='.$last_post_data['id'].', last_poster=\''.$db->escape($last_post_data['poster']).'\', num_replies=num_replies-1 WHERE id='.$topic_id) or error('Unable to update topic', __FILE__, __LINE__, $db->error());
+
+			// Get last_post, last_post_id, and last_poster from the new topic and update it
+			$result = $db->query('SELECT id, poster, posted FROM '.$db->prefix.'posts WHERE topic_id='.$new_tid.' ORDER BY id DESC LIMIT 1') or error('Unable to fetch post info', __FILE__, __LINE__, $db->error());
+			$last_post_data = $db->fetch_assoc($result);
+			$db->query('UPDATE '.$db->prefix.'topics SET last_post='.$last_post_data['posted'].', last_post_id='.$last_post_data['id'].', last_poster=\''.$db->escape($last_post_data['poster']).'\', num_replies=0 WHERE id='.$new_tid) or error('Unable to update topic', __FILE__, __LINE__, $db->error());
+
+			update_forum($post['forum_id']);
+			update_forum($pun_config['o_softdelete_forum']);
+		} else if ($pun_user['g_id'] == PUN_ADMIN) {
+			hard_delete_post($post_id, $topic_id);
+		}
+	} else {
+		hard_delete_post($post_id, $topic_id);
+	}
+}
+
+//
+// Delete a single post
+//
+function hard_delete_post($post_id, $topic_id)
 {
 	global $db;
 
---
+++ lang/German/admin_plugin_softdelete.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang_admin_plugin_softdelete = array(
+	'Invalid forum id'	=>	'Die angegebene Foren-ID ist kein gültiger positiver Zahlenwert.',
+	'Plugin title'		=>	'SoftDelete-Einrichtung',
+	'Saved'			=>	'Einstellungen gespeichert.',
+	'No forums'		=>	'Es gibt keine Foren zur Auswahl.',
+	'Explanation 1'		=>	'Dieses Plugin erlaubt dir, den SoftDelete-Mod einzurichten.',
+	'Form title'		=>	'Papierkorb',
+	'Legend text'		=>	'Wähle das Forum für den Papierkorb.',
+	'ForumField'		=>	'Forum',
+	'Disable'		=>	'Hartlöschen',
+	'Save'			=>	'Speichern',
+);
